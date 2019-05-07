91fd1265a56806320c223838facb2bbfc136d3ef
Taras Labiak <tlabiak@ukeess.com>
2019-05-07 17:48:03 +0300

    using ApplicationContext

--- src/main/java/org/labiak/java/controllers/HomeController.java
+++ src/main/java/org/labiak/java/controllers/HomeController.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 @RestController
@@ -18,15 +20,24 @@ public class HomeController {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private ApplicationContext context;
+
     @GetMapping(path = "/about")
     public ObjectNode main() {
         RestTemplate client = new RestTemplate();
         ResponseEntity<IpResponse> r = client.getForEntity("https://api.ipify.org?format=json", IpResponse.class);
         LocalDateTime d = LocalDateTime.now();
         ObjectNode object = mapper.createObjectNode();
+        LocalDateTime startup = LocalDateTime.ofEpochSecond(context.getStartupDate() / 1000, 0, ZoneOffset.ofHours(2));
         object.put("ok", true);
         object.put("time", DateTimeFormatter.ISO_DATE_TIME.format(d));
         object.put("ip", r.getBody().ip);
+        object.put("name", context.getApplicationName());
+        object.put("displayName", context.getDisplayName());
+        object.put("id", context.getId());
+        object.put("hasParent", context.getParent() != null);
+        object.put("startup", DateTimeFormatter.ISO_DATE_TIME.format(startup));
         return object;
     }
 
